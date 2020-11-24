3c6ec7a0085f2e9bae9f549d310e41ccadc5af70
Annalisa <annalisadesantis770@gmail.com>
2020-11-24 13:26:04 +0100

    ricerca con input nel api

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 var app = new Vue({
     el: "#root",
     data: {
+        filterTitle: "",
+        film: []
+    },
+    methods:{
+        search(){
+            axios
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params:{
+                        api_key: 'ff1d795e3b22f2a6056bd3125c445371',
+                        query: this.filterTitle
+                    }
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+            console.log(this.filterTitle);
+        }
 
     }
+
+
+
 
 });
