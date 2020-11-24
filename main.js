6141c825308435badef550ba242b1222174bc351
Annalisa <annalisadesantis770@gmail.com>
2020-11-24 18:28:29 +0100

    aggiunta commenti in main

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 var app = new Vue({
     el: "#root",
     data: {
+        // Input di ricerca
         filterTitle: "",
+        // Array film
         movies: [],
+        // Array serie tv
         serie: [],
+        // Array film + array serie tv
         allresults: [],
+        // Array flag disponibili
         availableFlags: ['it', 'en', 'es', 'fr', 'de', 'pt', 'zh', 'ru', 'ja']
     },
     methods:{
+        // Funzione per calcolare il voto in 5/5
         getFasStar(average) {
 
+            // Divido il valore voto
             let valore = average / 2;
+            // Arrotondo il valore per eccesso
             let arrotondato = Math.round(valore);
             return arrotondato;
         },
         search(){
+            // Chiamata get per i film
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params:{
                     api_key: 'ff1d795e3b22f2a6056bd3125c445371',
@@ -22,10 +31,13 @@ var app = new Vue({
                 }
             })
             .then((results) => {
+                // Assegno al risultato dell'api all'array movies
                 this.movies = results.data.results;
+                // Ripulisco l'input
                 this.filterTitle = "";
             });
 
+            // Chiamata get per le serie tv
             axios.get('https://api.themoviedb.org/3/search/tv', {
                 params:{
                     api_key: 'ff1d795e3b22f2a6056bd3125c445371',
@@ -33,8 +45,9 @@ var app = new Vue({
                 }
             })
             .then((results) => {
+                // Assegno al risultato dell'api all'array serie
                 this.serie = results.data.results;
-                this.filterTitle = "";
+                // Assegno ad uno nuovo array il risultato dei due array film e serie
                 this.allresults = this.movies.concat(this.serie);
                 console.log(this.allresults);
 
