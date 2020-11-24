f924d03c52d942d48ede3e053af81c17398275f3
Annalisa <annalisadesantis770@gmail.com>
2020-11-24 17:54:08 +0100

    tab in main

--- main.js
+++ main.js
@@ -15,17 +15,17 @@ var app = new Vue({
         },
         search(){
             axios
-            .get('https://api.themoviedb.org/3/search/movie', {
-                params:{
-                    api_key: 'ff1d795e3b22f2a6056bd3125c445371',
-                    query: this.filterTitle
-                }
-            })
-            .then((results) => {
-                this.movies = results.data.results;
-                console.log(this.movies);
-                this.filterTitle = "";
-            })
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params:{
+                        api_key: 'ff1d795e3b22f2a6056bd3125c445371',
+                        query: this.filterTitle
+                    }
+                })
+                .then((results) => {
+                    this.movies = results.data.results;
+                    console.log(this.movies);
+                    this.filterTitle = "";
+                })
 
         }
 
