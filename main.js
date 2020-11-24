619dd4b927a2ece2d4567e82ac24f01ef73789c7
Annalisa <annalisadesantis770@gmail.com>
2020-11-24 18:21:20 +0100

    ricerca serie tv integrata

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var app = new Vue({
         filterTitle: "",
         movies: [],
         serie: [],
+        allresults: [],
         availableFlags: ['it', 'en', 'es', 'fr', 'de', 'pt', 'zh', 'ru', 'ja']
     },
     methods:{
@@ -14,18 +15,30 @@ var app = new Vue({
             return arrotondato;
         },
         search(){
-            axios
-                .get('https://api.themoviedb.org/3/search/movie', {
-                    params:{
-                        api_key: 'ff1d795e3b22f2a6056bd3125c445371',
-                        query: this.filterTitle
-                    }
-                })
-                .then((results) => {
-                    this.movies = results.data.results;
-                    console.log(this.movies);
-                    this.filterTitle = "";
-                })
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params:{
+                    api_key: 'ff1d795e3b22f2a6056bd3125c445371',
+                    query: this.filterTitle
+                }
+            })
+            .then((results) => {
+                this.movies = results.data.results;
+                this.filterTitle = "";
+            });
+
+            axios.get('https://api.themoviedb.org/3/search/tv', {
+                params:{
+                    api_key: 'ff1d795e3b22f2a6056bd3125c445371',
+                    query: this.filterTitle
+                }
+            })
+            .then((results) => {
+                this.serie = results.data.results;
+                this.filterTitle = "";
+                this.allresults = this.movies.concat(this.serie);
+                console.log(this.allresults);
+
+            });
 
         }
 
